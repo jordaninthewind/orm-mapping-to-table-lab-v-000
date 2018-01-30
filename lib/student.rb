fbd80feee00af72b2fbbf877fa28fe71baefde2a
Jordan Kline <jordan.kline@gmail.com>
2018-01-30 22:30:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,7 @@ attr_reader :id
 
   def self.create_table
     sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS songs (
+    CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade TEXT
